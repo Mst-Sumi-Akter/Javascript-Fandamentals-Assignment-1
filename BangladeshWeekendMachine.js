d9c5adfa1d9479902bea3c46ee276b588899a4ac
Sumi Akter <sumiwdcse@gmail.com>
2026-09-02 09:46:25 +0600

    Task 2 running

---
+++ BangladeshWeekendMachine.js
@@ -0,0 +1,27 @@
+function getDayType(day){
+    const dayName = day.toLowerCase();
+
+    switch(dayName){
+        case 'friday':
+            return 'Weekend';
+        case 'saturday':
+            return 'Weekend';
+        case 'sunday':
+            return 'Working Day';
+            case 'monday' :
+                return 'Working Day';
+        case 'tuesday' :
+            return 'Working Day';
+        case 'wednesday' :
+            return 'Working Day';
+        case 'thursday' :
+            return 'Working Day';
+            default:
+                return 'Invalid Day';
+    }
+}
+
+console.log(getDayType("Friday"));
+console.log(getDayType("friday"));
+console.log(getDayType("MONDAY"));
+
